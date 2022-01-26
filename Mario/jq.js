9bd82b2a5a3e559d48dfc4f74a763784b0517b5d
Sooraj <soorajnair@students.aryacollege.me>
2022-01-26 18:05:59 +0530

    changed from 1d to 2d :D

--- Mario/jq.js
+++ Mario/jq.js
@@ -1,15 +1,27 @@
 
 $(document).ready(function(){
-    var move=0;
+    var move_left=0;
+    var move_top=0;
     $(document).keydown(function(a){
         var x=a;
         if(x.keyCode==37){
-            move=move-5;
-            $("#mario").css("left",move)
+            move_left=move_left-5;
+            $("#mario").css("left",move_left)
         }
         else if(x.keyCode==39){
-           move=move+5;
-           $("#mario").css("left",move)
+           move_left=move_left+5;
+           $("#mario").css("left",move_left)
         }
+        else if(x.keyCode==38){
+            move_top=move_top-5;
+            $("#mario").css("top",move_top)
+
+        }
+        else if(x.keyCode==40){
+            move_top=move_top+5;
+            $("#mario").css("top",move_top)
+
+        }
+
     })
 })
